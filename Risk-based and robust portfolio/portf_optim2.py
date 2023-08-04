8f804c34c1d999eb0f175f67dd13fb83400a0713
KendrickkLi <94262052+KendrickkLi@users.noreply.github.com>
2023-08-04 02:14:51 -0400

    Add files via upload

---
+++ Risk-based and robust portfolio/portf_optim2.py
@@ -0,0 +1,142 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import math
+
+# Complete the following functions
+def strat_buy_and_hold(x_init, cash_init, mu, Q, cur_prices):
+   x_optimal = x_init
+   cash_optimal = cash_init
+   return x_optimal, cash_optimal
+
+def strat_equally_weighted(x_init, cash_init, mu, Q, cur_prices):
+
+    return x_optimal, cash_optimal
+
+def strat_min_variance(x_init, cash_init, mu, Q, cur_prices):
+
+   return x_optimal, cash_optimal
+
+def strat_max_Sharpe(x_init, cash_init, mu, Q, cur_prices):
+
+   return x_optimal, cash_optimal
+
+def strat_equal_risk_contr(x_init, cash_init, mu, Q, cur_prices):
+
+   return x_optimal, cash_optimal
+
+def strat_lever_equal_risk_contr(x_init, cash_init, mu, Q, cur_prices):
+
+   return x_optimal, cash_optimal
+
+def strat_robust_optim(x_init, cash_init, mu, Q, cur_prices):
+
+   return x_optimal, cash_optimal
+
+# Input file
+input_file_prices = 'Daily_closing_prices.csv'
+
+# Read data into a dataframe
+df = pd.read_csv(input_file_prices)
+
+# Convert dates into array [year month day]
+def convert_date_to_array(datestr):
+    temp = [int(x) for x in datestr.split('/')]
+    return [temp[-1], temp[0], temp[1]]
+
+dates_array = np.array(list(df['Date'].apply(convert_date_to_array)))
+data_prices = df.iloc[:, 1:].to_numpy()
+dates = np.array(df['Date'])
+# Find the number of trading days in Nov-Dec 2019 and
+# compute expected return and covariance matrix for period 1
+day_ind_start0 = 0
+day_ind_end0 = len(np.where(dates_array[:,0]==2019)[0])
+cur_returns0 = data_prices[day_ind_start0+1:day_ind_end0,:] / data_prices[day_ind_start0:day_ind_end0-1,:] - 1
+mu = np.mean(cur_returns0, axis = 0)
+Q = np.cov(cur_returns0.T)
+
+# Remove datapoints for year 2019
+data_prices = data_prices[day_ind_end0:,:]
+dates_array = dates_array[day_ind_end0:,:]
+dates = dates[day_ind_end0:]
+
+# Initial positions in the portfolio
+init_positions = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 902, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17500])
+
+# Initial value of the portfolio
+init_value = np.dot(data_prices[0,:], init_positions)
+print('\nInitial portfolio value = $ {}\n'.format(round(init_value, 2)))
+
+# Initial portfolio weights
+w_init = (data_prices[0,:] * init_positions) / init_value
+
+# Number of periods, assets, trading days
+N_periods = 6*len(np.unique(dates_array[:,0])) # 6 periods per year
+N = len(df.columns)-1
+N_days = len(dates)
+
+# Annual risk-free rate for years 2020-2021 is 2.5%
+r_rf = 0.025
+# Annual risk-free rate for years 2008-2009 is 4.5%
+r_rf2008_2009 = 0.045
+
+# Number of strategies
+strategy_functions = ['strat_buy_and_hold', 'strat_equally_weighted', 'strat_min_variance', 'strat_max_Sharpe', 'strat_equal_risk_contr', 'strat_lever_equal_risk_contr', 'strat_robust_optim']
+strategy_names     = ['Buy and Hold', 'Equally Weighted Portfolio', 'Mininum Variance Portfolio', 'Maximum Sharpe Ratio Portfolio', 'Equal Risk Contributions Portfolio', 'Leveraged Equal Risk Contributions Portfolio', 'Robust Optimization Portfolio']
+N_strat = 1  # comment this in your code
+#N_strat = len(strategy_functions)  # uncomment this in your code
+fh_array = [strat_buy_and_hold, strat_equally_weighted, strat_min_variance, strat_max_Sharpe, strat_equal_risk_contr, strat_lever_equal_risk_contr, strat_robust_optim]
+
+portf_value = [0] * N_strat
+x = np.zeros((N_strat, N_periods),  dtype=np.ndarray)
+cash = np.zeros((N_strat, N_periods),  dtype=np.ndarray)
+for period in range(1, N_periods+1):
+   # Compute current year and month, first and last day of the period
+   if dates_array[0, 0] == 20:
+       cur_year  = 20 + math.floor(period/7)
+   else:
+       cur_year  = 2020 + math.floor(period/7)
+
+   cur_month = 2*((period-1)%6) + 1
+   day_ind_start = min([i for i, val in enumerate((dates_array[:,0] == cur_year) & (dates_array[:,1] == cur_month)) if val])
+   day_ind_end = max([i for i, val in enumerate((dates_array[:,0] == cur_year) & (dates_array[:,1] == cur_month+1)) if val])
+   print('\nPeriod {0}: start date {1}, end date {2}'.format(period, dates[day_ind_start], dates[day_ind_end]))
+   
+   # Prices for the current day
+   cur_prices = data_prices[day_ind_start,:]
+
+   # Execute portfolio selection strategies
+   for strategy  in range(N_strat):
+
+      # Get current portfolio positions
+      if period == 1:
+         curr_positions = init_positions
+         curr_cash = 0
+         portf_value[strategy] = np.zeros((N_days, 1))
+      else:
+         curr_positions = x[strategy, period-2]
+         curr_cash = cash[strategy, period-2]
+
+      # Compute strategy
+      x[strategy, period-1], cash[strategy, period-1] = fh_array[strategy](curr_positions, curr_cash, mu, Q, cur_prices)
+
+      # Verify that strategy is feasible (you have enough budget to re-balance portfolio)
+      # Check that cash account is >= 0
+      # Check that we can buy new portfolio subject to transaction costs
+
+      ###################### Insert your code here ############################
+
+      # Compute portfolio value
+      p_values = np.dot(data_prices[day_ind_start:day_ind_end+1,:], x[strategy, period-1]) + cash[strategy, period-1]
+      portf_value[strategy][day_ind_start:day_ind_end+1] = np.reshape(p_values, (p_values.size,1))
+      print('  Strategy "{0}", value begin = $ {1:.2f}, value end = $ {2:.2f}'.format( strategy_names[strategy], 
+             portf_value[strategy][day_ind_start][0], portf_value[strategy][day_ind_end][0]))
+
+      
+      # Compute expected returns and covariances for the next period
+      cur_returns = data_prices[day_ind_start+1:day_ind_end+1,:] / data_prices[day_ind_start:day_ind_end,:] - 1
+      mu = np.mean(cur_returns, axis = 0)
+      Q = np.cov(cur_returns.T)
+
+# Plot results
+###################### Insert your code here ############################
